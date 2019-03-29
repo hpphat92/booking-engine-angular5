4a7f4df4b83974dec3f1cb6060891373939cd0e8
bá khanh <khanhbvh@outlook.com.vn>
2019-03-29 09:43:44 +0700

    bidding

--- src/app/tour-guide/tour-guide-home/tour-guide-home.component.ts
+++ src/app/tour-guide/tour-guide-home/tour-guide-home.component.ts
@@ -5,6 +5,64 @@ import { Component } from '@angular/core';
   templateUrl: './tour-guide-home.component.html'
 })
 export class TourGuideHomeComponent {
+  shortSelfDescription: string;
+  aboutTitle: string;
+  aboutDescription: string;
+  images: any;
+  affiliationsRecognitions: any;
+  languages: any;
+  specializations: any;
+
   constructor() {
+    this.shortSelfDescription = "Fun, Outgoing and Adventurous! I am actually shy by nature, but I do warm up to people quickly!";
+    this.aboutTitle = 'Wherever you are, most welcome';
+    this.aboutDescription = '<div>Lorem ipsum dolor sit amet, consectetur adipiscing faucibus sem. Nulla dapibus. sed magna ac, convallis faucibus sem. Nulla at lorem ac arcu mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing' +
+      'faucibus sem. Nulla dapibus. sed magna ac, convallis faucibus sem. Nulla at lorem ac arcu mattis' +
+      'dapibus.</div>' +
+      '<br>' +
+      '<div>Lorem ipsum dolor sit amet, consectetur adipiscing faucibus sem. Nulla dapibus. sed magna ac, convallis' +
+      'faucibus sem. Nulla at lorem ac arcu mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing' +
+      'faucibus sem. Nulla dapibus. sed magna ac, convallis faucibus sem. Nulla at lorem ac arcu mattis' +
+      'dapibus.</div>' +
+      '<br>' +
+      '<div>Lorem ipsum dolor sit amet, consectetur adipiscing faucibus sem. Nulla dapibus. sed magna ac, convallis' +
+      'faucibus sem. Nulla at lorem ac arcu mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing' +
+      'faucibus sem. Nulla dapibus. sed magna ac, convallis faucibus sem. Nulla at lorem ac arcu mattis' +
+      'dapibus.</div>';
+    this.languages = ['English', 'Mandarin', 'Japanese', 'Indonesian'];
+    this.specializations = ['Food & Beverage', 'Historical & Cultural', 'Outdoor & Sightseeing'];
+    this.images = ['../../assets/images/useravatar.jpeg', '../../assets/images/useravatar.jpeg', '../../assets/images/useravatar.jpeg']
+    this.affiliationsRecognitions = [
+      {
+        image: '../../assets/images/useravatar.jpeg',
+        title: 'Selina Gorson',
+        shortDescription: 'Joined 5 years'
+      },
+      {
+        image: '../../assets/images/useravatar.jpeg',
+        title: 'Selina Gorson',
+        shortDescription: 'Joined 5 years'
+      },
+      {
+        image: '../../assets/images/useravatar.jpeg',
+        title: 'Selina Gorson',
+        shortDescription: 'Joined 5 years'
+      },
+      {
+        image: '../../assets/images/useravatar.jpeg',
+        title: 'Selina Gorson',
+        shortDescription: 'Joined 5 years'
+      },
+      {
+        image: '../../assets/images/useravatar.jpeg',
+        title: 'Selina Gorson',
+        shortDescription: 'Joined 5 years'
+      },
+      {
+        image: '../../assets/images/useravatar.jpeg',
+        title: 'Selina Gorson',
+        shortDescription: 'Joined 5 years'
+      },
+    ];
   }
 }
--- src/app/tour-guide/tour-guide.component.ts
+++ src/app/tour-guide/tour-guide.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./tour-guide.component.scss']
 })
 export class TourGuideComponent {
+  email: string;
+  number: string;
+
   constructor() {
+    this.email = "selina@trabble.co";
+    this.number = "+65 3688 8888";
   }
 }
